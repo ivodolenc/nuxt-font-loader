5ed9e60d1391ad6363528d09ba911f5784b9ee90
Ivo Dolenc <ivodolenc@gmail.com>
2023-11-27 21:37:35 +0100

    chore: update bundler config

---
+++ bundler.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from '@hypernym/bundler'
+import { name, version } from './package.json'
+import { nuxtMetaTemplate, nuxtTypesTemplate } from './src/utils/templates.js'
+
+export default defineConfig({
+  entries: [
+    {
+      input: './src/module.ts',
+      externals: [/^@nuxt/],
+      plugins: {
+        replace: {
+          preventAssignment: true,
+          __name__: name,
+          __version__: version,
+        },
+      },
+    },
+    {
+      types: './src/types/module.ts',
+      output: './dist/module.d.ts',
+    },
+    {
+      input: './src/runtime/composables/index.ts',
+      output: './dist/runtime/composables/index.mjs',
+      externals: ['#imports'],
+    },
+    {
+      types: './src/types/runtime/composables/index.ts',
+      output: './dist/runtime/composables/index.d.ts',
+    },
+    {
+      template: true,
+      output: './dist/module.json',
+      content: nuxtMetaTemplate(),
+      format: 'json',
+    },
+    {
+      template: true,
+      output: './dist/types.d.ts',
+      content: nuxtTypesTemplate(),
+      format: 'dts',
+    },
+  ],
+})
